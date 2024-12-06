bfc61d897e3dcb71df49789a5db0195cde012af5
Sarah Sanders <sarah.sanders@ucl.ac.uk>
2024-12-06 11:17:25 +0000

    Update queries.py

--- coursework2/queries.py
+++ coursework2/queries.py
@@ -3,14 +3,13 @@ import sqlite3
 from importlib import resources
 
 # See pyproject.toml which states that the package is coursework2 and that .db files are in that package
-# This is a constant so is capitalised
-DB_PATH = str(resources.files("coursework2").joinpath("paralympics.db"))
+db_path = str(resources.files("coursework2").joinpath("paralympics.db"))
 
 
 def select_all(table_name, columns):
     # You can't parameterise table and column names in SQLite, so string formatting used here
     qry = f"SELECT {columns} FROM {table_name};"
-    connection = sqlite3.connect(DB_PATH)
+    connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
     cursor.execute(qry)
     rows = cursor.fetchall()
